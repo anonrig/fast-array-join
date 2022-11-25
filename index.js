9eb91fc55c90af4c067a0fbf01791fb92b33aa3d
Igor Kalashnikov <igor.kalashnikov@me.com>
2022-11-25 10:30:02 +0300

    simplify join to oneline reduce

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 "use strict";
 
-function join(output, separator = ",") {
-  let str = "";
-  if (output.length !== 0) {
-    const lastIndex = output.length - 1;
-    for (let i = 0; i < lastIndex; i++) {
-      // It is faster not to use a template string here
-      str += output[i];
-      str += separator;
-    }
-    str += output[lastIndex];
-  }
-  return str;
-}
+const join = (output, separator = ",") =>
+  output.reduce((result, chunk) => result + separator + chunk, "");
 
 module.exports = join;
 module.exports.default = join;
